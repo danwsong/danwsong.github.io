9b740e3ea3e2138df9b7e542ce6106b77d7000ac
Daniel Song <danwsong@gmail.com>
2019-05-21 23:07:54 -0400

    separate into containers and redesign layout

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 new TypeIt('.greeting', {
-    speed: 250,
+    speed: 125,
     lifeLike: true,
-    cursorChar: '_',
+    cursorChar: '|',
     startDelay: 1000
-  })
-  .type("<span class=\"tag\">&lt;h1&gt; </span>hey! i'm <span class=\"name\">dan</span>.<span class=\"tag\"> &lt;/h1&gt;</span>")
-  .go();
+})
+    .type("hey! i'm <span class=\"name\">dan</span>.")
+    .go();
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1000, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
